432b66d06d8dd9f8ab8edfc79b2e9f83f3b97515
Taoufik KESKES <taokes@gmail.com>
2022-10-20 15:27:24 +0200

    Create testchild.sh

---
+++ testchild.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+## Adding a new file in child branch
+echo "Inside Child branch"
+# after save the code use chmod 755 file_name.
+#insead of 755, you can use +x means to give permission for execution.
+#or you can directly use bash file_name
